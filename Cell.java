e9db993f84af76992005e0c8498fbec7f1ba50f2
shiragri <shiragrinvald@gmail.com>
2024-12-26 20:18:47 +0200

    define indOfMainOp method

--- Cell.java
+++ Cell.java
@@ -5,19 +5,31 @@ public class Cell {
     private String strVal;
     private String formVal;
     private String value;
+
     public Cell(String str) {
         this.value = str;
 
     }
 
-    public boolean isNumber(){
-       try {
-           int a =Integer.parseInt(value);
-           return true;
-       }
-       catch (NumberFormatException e){
-           return false;
-       }
+
+
+    private boolean isNumber(String str) {
+
+        try {
+            int a =Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+
+
+    }
+
+    public boolean isNumber() {
+
+        return isNumber(value);
+
     }
 
     public boolean isText(){
@@ -29,7 +41,44 @@ public class Cell {
         return value.charAt(0) == '0';
     }
 
-    public Double computeForm(String form){
-        return null;
+    public double computeForm(String form){
+        int opIndex = indOfMainOp(form);
+        String leftSide = form.substring(0, opIndex);
+        String rightSide = form.substring(opIndex+1);
+        char op = form.charAt(opIndex);
+
+        double leftSideVal;
+        if (isNumber(leftSide))
+            leftSideVal = Double.parseDouble(leftSide);
+        else
+            leftSideVal = computeForm (leftSide);
+
+        double rightSideVal;
+        if (isNumber(rightSide))
+            rightSideVal = Double.parseDouble(rightSide);
+        else
+            rightSideVal = computeForm (rightSide);
+
+        double retVal=-1;
+        if (op == '+')
+            retVal= leftSideVal + rightSideVal;
+        else if (op == '-')
+            retVal= leftSideVal - rightSideVal;
+        else if (op == '*')
+            retVal= leftSideVal * rightSideVal;
+        else if (op == '/')
+            retVal= leftSideVal / rightSideVal;
+
+
+        return retVal;
+
+
+       // return null;
+    }
+    private int indOfMainOp(String form){
+        for (int i = form.length(); i > 0; i--) {
+            if (form.charAt(i-1) == '+' || form.charAt(i-1) == '-');
+        }
+        return 4;
     }
 }
