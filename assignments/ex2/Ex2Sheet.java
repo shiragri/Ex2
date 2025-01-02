7dd94b60e1a929e8d476717960b851c82d17a29c
shiragri <shiragrinvald@gmail.com>
2025-01-02 17:06:30 +0200

    .

--- assignments/ex2/Ex2Sheet.java
+++ assignments/ex2/Ex2Sheet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 public class Ex2Sheet implements Sheet {
     private Cell[][] table;
+    private double [][] values;
     // Add your code here
 
     // ///////////////////
@@ -68,8 +69,30 @@ public class Ex2Sheet implements Sheet {
     @Override
     public void eval() {
         int[][] dd = depth();
+
         // Add your code here
 
+        values = new double[width()][height()];
+
+        for (int x = 0; x < width(); x++) {
+            for (int y = 0; y < height(); y++) {
+                String ans = null;
+                if(get(x,y)!=null) {
+                    ans = get(x,y).toString();}
+                // Add your code here
+                Cell c =get(x,y);
+                int type=c.getType();
+                if (type== Ex2Utils.FORM)
+                    return values[x][y]=computeForm(ans);
+
+               // else if (type==Ex2Utils.TEXT)
+                //    return ans;
+               // else return Double.toString(computeForm(ans));
+
+                values[x][y] = ans;
+
+            }
+        }
         // ///////////////////
     }
 
@@ -107,12 +130,10 @@ public class Ex2Sheet implements Sheet {
 
     @Override
     public String eval(int x, int y) {
-        String ans = null;
-        if(get(x,y)!=null) {ans = get(x,y).toString();}
-        // Add your code here
+
 
         /////////////////////
-        return ans;
+      //  return ans;
         }
 
     public int xCell(String x) {
@@ -140,5 +161,94 @@ public class Ex2Sheet implements Sheet {
         }
 
         return a;
+    }
+
+    public boolean isNumber(String str) {
+
+        try {
+            int a =Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+    public double computeForm(String form){
+        String form1 = "";
+        if (form.charAt(0)== '=')
+            form1 = form.substring(1);
+        else
+            form1 = form;
+        if (form1.charAt(0) == '(' && form1.charAt(form1.length()-1) == ')')
+            form1 = form1.substring(1, form1.length()-1);
+
+        int opIndex = indOfMainOp(form1);
+        String leftSide = form1.substring(0, opIndex);
+        String rightSide = form1.substring(opIndex+1);
+        char op = form1.charAt(opIndex);
+
+        double leftSideVal;
+        if (isNumber(leftSide))
+            leftSideVal = Double.parseDouble(leftSide);
+        /*else if (isCell(leftSide)) {
+           int x= excell.xCell(str);
+           int y= excell.yCell(str);
+           Cell cell =excell.get(x,y);
+            leftSideVal= cell.computeForm("3+5");
+
+        }
+        */
+        else
+            leftSideVal = computeForm (leftSide);
+
+        double rightSideVal;
+        if (isNumber(rightSide))
+            rightSideVal = Double.parseDouble(rightSide);
+        else
+            rightSideVal = computeForm (rightSide);
+
+        double retVal=-1;
+        if (op == '+')
+            retVal= leftSideVal + rightSideVal;
+        else if (op == '-')
+            retVal= leftSideVal - rightSideVal;
+        else if (op == '*')
+            retVal= leftSideVal * rightSideVal;
+        else if (op == '/')
+            retVal= leftSideVal / rightSideVal;
+
+
+        return retVal;
+
+
+        // return null;
+    }
+
+    private  int indOfMainOp(String form){
+        int opIndex = -1;
+        boolean isInParenthesis = false;
+        int parenthesisCounter = 0;
+        for (int i = form.length()-1; i > 0; i--) {
+            if (form.charAt(i) == ')') {
+                isInParenthesis = true;
+                parenthesisCounter++;
+            }
+            else if (form.charAt(i) == '(') {
+                parenthesisCounter--;
+                if (parenthesisCounter == 0)
+                    isInParenthesis = false;
+
+            }
+
+
+            if (!isInParenthesis) {
+                if (form.charAt(i) == '+' || form.charAt(i) == '-')
+                    return i;
+                else if ((form.charAt(i) == '*' || form.charAt(i) == '/') && opIndex == -1)
+                    opIndex = i;
+            }
+        }
+        return opIndex;
     }
 }
--- assignments/ex2/SCell.java
+++ assignments/ex2/SCell.java
@@ -70,8 +70,6 @@ public void setData(String s) {
         catch (NumberFormatException e){
             return false;
         }
-
-
     }
 
     public boolean isText(String str){
@@ -80,84 +78,11 @@ public void setData(String s) {
     }
 
     public boolean isForm(String str){
-        return str.charAt(0) == '=';
+       if (str != null && str.length() > 0) {
+           return str.charAt(0) == '=';
+       }
+       else return false;
     }
 
-    public double computeForm(String form){
-        String form1 = "";
-        if (form.charAt(0)== '=')
-            form1 = form.substring(1);
-        else
-            form1 = form;
-        if (form1.charAt(0) == '(' && form1.charAt(form1.length()-1) == ')')
-            form1 = form1.substring(1, form1.length()-1);
 
-        int opIndex = indOfMainOp(form1);
-        String leftSide = form1.substring(0, opIndex);
-        String rightSide = form1.substring(opIndex+1);
-        char op = form1.charAt(opIndex);
-
-        double leftSideVal;
-        if (isNumber(leftSide))
-            leftSideVal = Double.parseDouble(leftSide);
-        /*else if (isCell(leftSide)) {
-           int x= excell.xCell(str);
-           int y= excell.yCell(str);
-           Cell cell =excell.get(x,y);
-            leftSideVal= cell.computeForm("3+5");
-
-        }
-        */
-        else
-            leftSideVal = computeForm (leftSide);
-
-        double rightSideVal;
-        if (isNumber(rightSide))
-            rightSideVal = Double.parseDouble(rightSide);
-        else
-            rightSideVal = computeForm (rightSide);
-
-        double retVal=-1;
-        if (op == '+')
-            retVal= leftSideVal + rightSideVal;
-        else if (op == '-')
-            retVal= leftSideVal - rightSideVal;
-        else if (op == '*')
-            retVal= leftSideVal * rightSideVal;
-        else if (op == '/')
-            retVal= leftSideVal / rightSideVal;
-
-
-        return retVal;
-
-
-        // return null;
-    }
-
-    private  int indOfMainOp(String form){
-        int opIndex = -1;
-        boolean isInParenthesis = false;
-        int parenthesisCounter = 0;
-        for (int i = form.length()-1; i > 0; i--) {
-            if (form.charAt(i) == ')') {
-                isInParenthesis = true;
-                parenthesisCounter++;
-            }
-            else if (form.charAt(i) == '(') {
-                parenthesisCounter--;
-                if (parenthesisCounter == 0)
-                    isInParenthesis = false;
-
-            }
-
-
-            if (!isInParenthesis) {
-                if (form.charAt(i) == '+' || form.charAt(i) == '-')
-                    return i;
-                else if ((form.charAt(i) == '*' || form.charAt(i) == '/') && opIndex == -1)
-                    opIndex = i;
-            }
-        }
-        return opIndex;
-    }
 }
